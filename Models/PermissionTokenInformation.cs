31b5db56a69a54dc21f151c9f7070a3d66a125eb
Alastair <alastair.w.little@gmail.com>
2022-02-09 22:50:14 +0000

    Remove unused model

--- Models/PermissionTokenInformation.cs
+++
@@ -1,8 +0,0 @@
-namespace EncryptionHandler.Models;
-
-    public class PermissionTokenInformation
-    {
-        public Guid UserId { get; set; }
-        public string Area { get; set; } = null!;
-        public string Action { get; set; } = null!;
-    }
